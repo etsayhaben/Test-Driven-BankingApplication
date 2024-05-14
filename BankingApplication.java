861a75629e0aec7f5e48d565b3f639f6f6f915cf
haben etsay <etsayhaben@gmail.com>
2024-05-14 21:31:54 +0300

    creation of account is finished

---
+++ BankingApplication.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankingApplication {
+    private Map<String, Double> accounts;
+
+    public BankingApplication() {
+        accounts = new HashMap<>();
+    }
+
+    public void createAccount(String accountNumber, double initialBalance) {
+        if (!accounts.containsKey(accountNumber)) {
+            accounts.put(accountNumber, initialBalance);
+        }
+    }
+
+    public double checkBalance(String accountNumber) {
+        return accounts.getOrDefault(accountNumber, 0.0);
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        if (accounts.containsKey(accountNumber)) {
+            double currentBalance = accounts.get(accountNumber);
+            accounts.put(accountNumber, currentBalance + amount);
+        }
+    }
+
+    public void closeAccount(String accountNumber) {
+        accounts.remove(accountNumber);
+    }
+
+    public static void main(String args[]) {
+        new BankingApplication();
+    }
+
+}
---
+++ BankingApplicationTest.java
@@ -0,0 +1,52 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BankingApplicationTest {
+    private BankingApplication bankingApp;
+
+    @Before
+    public void setUp() {
+        bankingApp = new BankingApplication();
+    }
+
+    @Test
+    public void testCreateAccountWithInitialBalance() {
+        // Test creating an account with an initial balance
+        bankingApp.createAccount("1234567890", 1000.0);
+        double balance = bankingApp.checkBalance("1234567890");
+        assertEquals(1000.0, balance, 0.01);
+    }
+
+    @Test
+    public void testCreateAccountWithExistingAccountNumber() {
+        // Test creating an account with an existing account number
+        // The account creation should fail and maintain the original balance
+        bankingApp.createAccount("1234567890", 1000.0);
+        bankingApp.createAccount("1234567890", 2000.0);
+        double balance = bankingApp.checkBalance("1234567890");
+        assertEquals(1000.0, balance, 0.01);
+    }
+
+    @Test
+    public void testDepositIntoExistingAccount() {
+        // Test depositing into an existing account
+        bankingApp.createAccount("1234567890", 1000.0);
+        bankingApp.deposit("1234567890", 500.0);
+        double balance = bankingApp.checkBalance("1234567890");
+        assertEquals(1500.0, balance, 0.01);
+        assertNotNull(bankingApp.checkBalance("1234567890"));
+    }
+
+    @After
+    public void tearDown() {
+        // Clean up resources after each test
+    }
+
+    @AfterClass
+    public static void tearDownAll() {
+        // Clean up resources once after all tests have been executed
+    }
+}
