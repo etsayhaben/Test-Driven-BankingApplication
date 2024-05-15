6f1998a2ec52d627746181f0ede1c11f8f6af8df
afthegreat <abelferede916@gmail.com>
2024-05-15 22:56:48 +0300

    edited and transfer full code with test case

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,5 +1,3 @@
-
-// this is the comment session of the code
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,20 +5,22 @@ public class BankingApplication {
     private Map<String, Double> accounts;
 
     public BankingApplication() {
-        accounts = new HashMap<>();
+        this.accounts = new HashMap<>();
     }
 
-    // the following code works on creating new account.
+    // Method to create a new account
     public void createAccount(String accountNumber, double initialBalance) {
         if (!accounts.containsKey(accountNumber)) {
             accounts.put(accountNumber, initialBalance);
         }
     }
 
+    // Method to check balance of an account
     public double checkBalance(String accountNumber) {
         return accounts.getOrDefault(accountNumber, 0.0);
     }
 
+    // Method to withdraw funds from an account
     public void withdraw(String accountNumber, double amount) {
         if (accounts.containsKey(accountNumber)) {
             double currentBalance = accounts.get(accountNumber);
@@ -30,6 +30,7 @@ public class BankingApplication {
         }
     }
 
+    // Method to deposit funds into an account
     public void deposit(String accountNumber, double amount) {
         if (accounts.containsKey(accountNumber)) {
             double currentBalance = accounts.get(accountNumber);
@@ -37,12 +38,28 @@ public class BankingApplication {
         }
     }
 
+    // Method to close an account
     public void closeAccount(String accountNumber) {
         accounts.remove(accountNumber);
+    }
+
+    // Method to transfer funds from one account to another
+    public void transfer(String fromAccount, String toAccount, double amount) {
+        if (accounts.containsKey(fromAccount) && accounts.containsKey(toAccount)) {
+            double fromBalance = accounts.get(fromAccount);
+            double toBalance = accounts.get(toAccount);
+            if (fromBalance >= amount) {
+                accounts.put(fromAccount, fromBalance - amount);
+                accounts.put(toAccount, toBalance + amount);
+            } else {
+                System.out.println("Insufficient balance to transfer " + amount);
+            }
+        } else {
+            System.out.println("One or both accounts do not exist.");
+        }
     }
 
     public static void main(String args[]) {
         new BankingApplication();
     }
-
 }
--- BankingApplicationTest.java
+++ BankingApplicationTest.java
@@ -4,10 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-// Assuming BankingApplication class is in com.yourpackage package
-import com.yourpackage.BankingApplication;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankingApplicationTest {
+
     private BankingApplication bankingApp;
 
     @Before
@@ -40,10 +41,31 @@ public class BankingApplicationTest {
         bankingApp.deposit("1234567890", 500.0);
         double balance = bankingApp.checkBalance("1234567890");
         assertEquals(1500.0, balance, 0.01);
-        assertNotNull(bankingApp.checkBalance("1234567890"));
     }
 
-    // Additional test cases can be added here
+    @Test
+    public void testWithdrawFromExistingAccount() {
+        // Test withdrawing from an existing account
+        bankingApp.createAccount("1234567890", 1000.0);
+        bankingApp.withdraw("1234567890", 500.0);
+        double balance = bankingApp.checkBalance("1234567890");
+        assertEquals(500.0, balance, 0.01);
+    }
+
+    @Test
+    public void testTransferBetweenAccounts() {
+        // Test transferring funds from one account to another
+        bankingApp.createAccount("1234567890", 2000.0);
+        bankingApp.createAccount("0987654321", 1000.0);
+
+        bankingApp.transfer("1234567890", "0987654321", 500.0);
+
+        double fromBalance = bankingApp.checkBalance("1234567890");
+        double toBalance = bankingApp.checkBalance("0987654321");
+
+        assertEquals(1500.0, fromBalance, 0.01);
+        assertEquals(1500.0, toBalance, 0.01);
+    }
 
     @After
     public void tearDown() {
@@ -53,5 +75,55 @@ public class BankingApplicationTest {
     @AfterClass
     public static void tearDownAll() {
         // Clean up resources once after all tests have been executed
+    }
+
+    // Class definition for BankingApplication
+    private class BankingApplication {
+        private Map<String, Double> accounts;
+
+        public BankingApplication() {
+            this.accounts = new HashMap<>();
+        }
+
+        public void createAccount(String accountNumber, double initialBalance) {
+            if (!accounts.containsKey(accountNumber)) {
+                accounts.put(accountNumber, initialBalance);
+            }
+        }
+
+        public double checkBalance(String accountNumber) {
+            return accounts.getOrDefault(accountNumber, 0.0);
+        }
+
+        public void withdraw(String accountNumber, double amount) {
+            if (accounts.containsKey(accountNumber)) {
+                double currentBalance = accounts.get(accountNumber);
+                if (currentBalance >= amount) {
+                    accounts.put(accountNumber, currentBalance - amount);
+                }
+            }
+        }
+
+        public void deposit(String accountNumber, double amount) {
+            if (accounts.containsKey(accountNumber)) {
+                double currentBalance = accounts.get(accountNumber);
+                accounts.put(accountNumber, currentBalance + amount);
+            }
+        }
+
+        public void transfer(String fromAccount, String toAccount, double amount) {
+            if (accounts.containsKey(fromAccount) && accounts.containsKey(toAccount)) {
+                double fromBalance = accounts.get(fromAccount);
+                double toBalance = accounts.get(toAccount);
+                if (fromBalance >= amount) {
+                    accounts.put(fromAccount, fromBalance - amount);
+                    accounts.put(toAccount, toBalance + amount);
+                } else {
+                    System.out.println("Insufficient balance to transfer " + amount);
+                }
+            } else {
+                System.out.println("One or both accounts do not exist.");
+            }
+        }
     }
 }
