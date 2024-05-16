7efe3e483426307b004ac092ab9ffc6b22acd4de
HABEN ETSAY <etsayhaben@gmail.com>
2024-05-16 11:11:20 +0300

    i have finsihed creating account method

--- BankingApplication.java
+++ BankingApplication.java
@@ -3,12 +3,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BankingApplication {
+    private Map<String, Double> accounts;
 
     public BankingApplication() {
+        accounts = new HashMap<>();
     }
 
     public static void main(String args[]) {
         new BankingApplication();
+    }
+
+    public void createAccount(String accountNumber, double initialBalance) {
+        if (!accounts.containsKey(accountNumber)) {
+            accounts.put(accountNumber, initialBalance);
+        }
     }
 
 }
