5ed2d5bc3fea1065788750fd5f4e3aa2f381e7f7
efrem <etsayhaben@gmail.com>
2024-05-16 10:52:00 +0300

    test case creating account with intial balance

--- BankingApplication.java
+++ BankingApplication.java
@@ -3,24 +3,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BankingApplication {
-    private Map<String, Double> accounts;
 
     public BankingApplication() {
-        accounts = new HashMap<>();
     }
-
-    //TRYING TO NEW BARNCH
-
-    public void createAccount(String accountNumber, double initialBalance) {
-        if (!accounts.containsKey(accountNumber)) {
-            accounts.put(accountNumber, initialBalance);
-        }
-    }
-
-    public double checkBalance(String accountNumber) {
-        return accounts.getOrDefault(accountNumber, 0.0);
-    }
-    
 
     public static void main(String args[]) {
         new BankingApplication();
